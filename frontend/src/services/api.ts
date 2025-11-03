d364b72c474363177541c29155f0d1cfae7c6c26
Kartikay Singh <kartikay.singh.0211@gmail.com>
2025-11-03 23:46:46 +0530

    updates

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,10 +2,11 @@ import axios from "axios";
 import type { Note } from "../types/Note";
 
 const API = axios.create({
-	baseURL: import.meta.env.VITE_API_URL,
+	baseURL: import.meta.env.VITE_API_URL, // should end with /api
 });
 
-export const getNotes = () => API.get<Note[]>("/");
-export const createNote = (note: Note) => API.post("/", note);
-export const updateNote = (id: number, note: Note) => API.put(`/${id}`, note);
-export const deleteNote = (id: number) => API.delete(`/${id}`);
+export const getNotes = () => API.get<Note[]>("/notes");
+export const createNote = (note: Note) => API.post("/notes", note);
+export const updateNote = (id: number, note: Note) =>
+	API.put(`/notes/${id}`, note);
+export const deleteNote = (id: number) => API.delete(`/notes/${id}`);
